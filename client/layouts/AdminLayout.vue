02e3933fbf89417e9347415dda19e0b49f84ba7a
Mikhail A <mg@abramishvili.net>
2021-10-22 01:33:47 +0500

    .

---
+++ client/layouts/AdminLayout.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <Nuxt />
+    </div>
+</template>
+<script>
+export default {
+    data: function () {
+        return {
+        };
+    },
+    created() {       
+    },
+    methods: {
+    },
+}
+</script>
--- client/pages/login.vue
+++ client/pages/login.vue
@@ -1,19 +1,29 @@
 <template>
-    <div class="container">
-        <a @click="login()">Login</a>
-
-        <NuxtLink to="/">Home Page</NuxtLink>
+    <div class="container mt-4">
+        <div class="row">
+            <div v-if="$auth.loggedIn">Вы успешно вошли ;)</div>
+            <div v-else class="col-12 col-md-6 offset-3">
+                <label class="form-label">Логин</label>
+                <input v-model="email" type="email" class="form-control mb-4" placeholder="Логин">
+                
+                <label class="form-label">Пароль</label>
+                <input v-model="password" type="password" class="form-control mb-4" placeholder="Пароль">
+                <button @click="login()" class="btn btn-standard">Войти</button>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
+    layout: 'AdminLayout',
     head: {
         title: 'Авторизация',
     },
     data: function () {
         return {
-            name: '',
+            email: '',
+            password: '',
         };
     },
     created() {
@@ -28,8 +38,8 @@ export default {
         login() {
             this.$auth.loginWith('laravelSanctum', {
                 data: {
-                    email: 'mg@abramishvili.net',
-                    password: '63VBtbjw'
+                    email: this.email,
+                    password: this.password
                 }
             })
             .then(() => 
